54aebded9f6cac5c35e18dc154f7bf8cf0ab1879
Hiram K. <hiram.kamau@outlook.com>
2019-12-23 15:52:31 +0300

    Redesign class

--- mecanico/src/main/java/com/github/idelstak/mecanico/domain/spi/Person.java
+++ mecanico/src/main/java/com/github/idelstak/mecanico/domain/spi/Person.java
@@ -16,26 +16,154 @@
  */
 package com.github.idelstak.mecanico.domain.spi;
 
+import com.github.idelstak.mecanico.domain.api.Address;
+import java.util.Objects;
+import java.util.Optional;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  *
  * @author Hiram K. <hiram.kamau@outlook.com>
  */
-public interface Person {
+public class Person implements ReadOnlyPersonProperties {
 
-    String getName();
+    private final ReadOnlyStringWrapper namePropertyWrapper;
+    private final ReadOnlyObjectWrapper<Address> addressPropertyWrapper;
+    private final ReadOnlyStringWrapper phonePropertyWrapper;
+    private final ReadOnlyStringWrapper emailPropertyWrapper;
 
-    void setName(String name);
+    /**
+     *
+     * @param name
+     * @param address
+     * @param phone
+     * @param email
+     *
+     * @throws IllegalArgumentException if the {@code name} is null
+     */
+    protected Person(String name, Address address, String phone, String email)
+            throws IllegalArgumentException {
 
-    Address getAddress();
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("Name should not be null nor empty");
+        }
 
-    void setAddress(Address address);
+        this.namePropertyWrapper = new ReadOnlyStringWrapper(name);
+        this.addressPropertyWrapper = new ReadOnlyObjectWrapper<>(address);
+        this.phonePropertyWrapper = new ReadOnlyStringWrapper(phone);
+        this.emailPropertyWrapper = new ReadOnlyStringWrapper(email);
+    }
 
-    String getPhone();
+    public static Person of(String name, Address address, String phone, String email) {
+        return new Person(name, address, phone, email);
+    }
 
-    void setPhone(String number);
+    public static Person from(String name) {
+        return new Person(name, null, "", "");
+    }
 
-    String getEmail();
+    @Override
+    public ReadOnlyStringProperty nameProperty() {
+        return namePropertyWrapper.getReadOnlyProperty();
+    }
 
-    void setEmail(String email);
+    @Override
+    public ReadOnlyObjectProperty<Address> addressProperty() {
+        return addressPropertyWrapper.getReadOnlyProperty();
+    }
+
+    @Override
+    public ReadOnlyStringProperty phoneProperty() {
+        return phonePropertyWrapper.getReadOnlyProperty();
+    }
+
+    @Override
+    public ReadOnlyStringProperty emailProperty() {
+        return emailPropertyWrapper.getReadOnlyProperty();
+    }
+
+    public String getName() {
+        return namePropertyWrapper.get();
+    }
+
+    public void setName(String name) {
+        if (StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("Name should not be null nor empty");
+        }
+        namePropertyWrapper.set(name);
+    }
+
+    public Optional<Address> getAddress() {
+        return Optional.ofNullable(addressPropertyWrapper.get());
+    }
+
+    public void setAddress(Address address) {
+        addressPropertyWrapper.set(address);
+    }
+
+    public Optional<String> getPhone() {
+        return Optional.ofNullable(phonePropertyWrapper.get());
+    }
+
+    public void setPhone(String phone) {
+        phonePropertyWrapper.set(phone);
+    }
+
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(emailPropertyWrapper.get());
+    }
+
+    public void setEmail(String email) {
+        emailPropertyWrapper.set(email);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.getName());
+        hash = 17 * hash + Objects.hashCode(this.getAddress());
+        hash = 17 * hash + Objects.hashCode(this.getPhone());
+        hash = 17 * hash + Objects.hashCode(this.getEmail());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        var other = (Person) obj;
+        if (!Objects.equals(this.getName(), other.getName())) {
+            return false;
+        }
+        if (!Objects.equals(this.getAddress(), other.getAddress())) {
+            return false;
+        }
+        if (!Objects.equals(this.getPhone(), other.getPhone())) {
+            return false;
+        }
+        return Objects.equals(this.getEmail(), other.getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder("------------------------")
+                .append(String.format("\nName: %s", getName()))
+                .append(String.format("\nAddress: \n%s", getAddress().orElse(Address.EMPTY)))
+                .append(String.format("\nPhone: %s", getPhone().orElse("N/A")))
+                .append(String.format("\nEmail: %s", getEmail().orElse("N/A")))
+                .append("\n------------------------")
+                .toString();
+    }
 
 }
