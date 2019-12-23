82b205e96bed3a5f4c936866d08b3a32e3090c01
Hiram K. <hiram.kamau@outlook.com>
2019-12-23 13:11:59 +0300

    Add Apache Commons dependency

--- mecanico/src/main/java/module-info.java
+++ mecanico/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.github.idelstak.mecanico {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.commons.lang3;
 
     opens com.github.idelstak.mecanico to javafx.fxml;
     exports com.github.idelstak.mecanico;
